879ef3bfffed3f524c2232276dd53e673a0bfa0e
Atmaj <mishraatmaj@gmai.com>
2023-03-25 08:35:28 +0530

    fill_filter_js

--- 12_fill_and_filter/01_fill_in_js.js
+++ 12_fill_and_filter/01_fill_in_js.js
@@ -5,3 +5,9 @@ arr1.fill(0)
 console.log(arr1);
 // arr1=arr1.fill(0);
 // console.log(arr1);
+
+// fill function fill the whole array with the the input that you give and updates the arrray
+
+const l=[2,3,4,5,6,7,8,9,0]
+
+console.log(l.fill("k", 2, 5));
